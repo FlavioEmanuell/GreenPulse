3b55e6488b70db3adcf5bdec3552227e1fb1f70b
FlavioEmanuell <bilolinha10@gmail.com>
2025-07-16 11:56:54 -0300

    deploy teste v5

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,22 +1,29 @@
 const sgMail = require("@sendgrid/mail");
 
 exports.handler = async (event) => {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-    try {
-        const { name, email, message } = JSON.parse(event.body);
+  try {
+    const { name, email, message } = JSON.parse(event.body);
 
-        const msg = {
-            to: "agrogreenpulse@gmail.com",  // ALTERE PARA O EMAIL QUE DEVE RECEBER
-            from: "agrogreenpulse@gmail.com", // Precisa ser um email autorizado no SendGrid
-            subject: `Novo contato de ${name}`,
-            text: `Nome: ${name}\nEmail: ${email}\nMensagem: ${message}`,
-        };
+    const msg = {
+      to: "agrogreenpulse@gmail.com", // Para onde vai o email
+      from: "agrogreenpulse@gmail.com", // Precisa ser um remetente verificado no SendGrid
+      replyTo: email, // Aqui entra o e-mail de quem preencheu o formulário
+      subject: `Novo contato de ${name}`,
+      text: `Nome: ${name}\nEmail: ${email}\nMensagem: ${message}`,
+    };
 
-        await sgMail.send(msg);
+    await sgMail.send(msg);
 
-        return { statusCode: 200, body: JSON.stringify({ message: "E-mail enviado com sucesso!" }) };
-    } catch (error) {
-        return { statusCode: 500, body: JSON.stringify({ error: error.toString() }) };
-    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "E-mail enviado com sucesso!" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.toString() }),
+    };
+  }
 };
